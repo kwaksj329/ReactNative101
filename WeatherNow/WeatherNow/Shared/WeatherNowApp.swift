2cd4cf6c910769d7ba9b577fa581db8c1bc0ac88
kwaksj329 <kwaksj329@gmail.com>
2022-07-01 01:35:14 +0900

    2.4 Layout System

---
+++ WeatherNow/WeatherNow/Shared/WeatherNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNowApp.swift
+//  Shared
+//
+//  Created by Kwak Sujong on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
